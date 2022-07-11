a2f9cde5e84444515af39e443001321d0adfbf40
Cedric Arisdakessian <carisdak@hawaii.edu>
2022-07-11 07:11:07 +0000

    print in test

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -104,6 +104,7 @@ def test_fill_gaps():
     cov.fill_gaps(2)
 
     na_arc = [arc for arc in cov.arcs if "@" in arc.qacc]
+    print(cov)
     assert len(na_arc) == 1
     assert na_arc[0].bounds() == (7, 12)
     assert cov.arcs[0].bounds() == (0, 3)
